51a51515b10d239af7622cd0ad8a87848f8dbe08
Salih Erdoğan <salihsel@gmail.com>
2022-08-06 10:34:04 +0300

    Generetor

---
+++ Assets/Scripts/SampeFever/Item.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SampleFever;
+
+public class Item : MonoBehaviour
+{
+    internal BaseItem myItem;
+    public void addSome(){
+        
+    }
+    
+    
+}
